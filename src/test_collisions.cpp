e6c8a25017e03a1f405e248a58e154be371c608b
Sam Mason <sam@samason.uk>
2021-09-07 11:18:39 +0100

    added some test code for figuring out how OCCT boolean ops work

--- src/test_collisions.cpp
+++ src/test_collisions.cpp
@@ -4,6 +4,7 @@
 #include <TopoDS.hxx>
 #include <TopoDS_Face.hxx>
 #include <TopoDS_Edge.hxx>
+#include <TopoDS_Shape.hxx>
 #include <TopoDS_Iterator.hxx>
 
 #include <TopExp_Explorer.hxx>
@@ -19,24 +20,76 @@
 #include "document.hpp"
 
 
-
-int
-main()
+void
+test_trivial_union()
 {
 	const TopoDS_Shape
 		s1 = BRepPrimAPI_MakeBox(gp_Pnt(10, 10, 10), 10, 10, 10).Shape(),
-		s2 = BRepPrimAPI_MakeBox(gp_Pnt(10, 10, 10), 10, 10, 10).Shape();
+		s2 = BRepPrimAPI_MakeBox(gp_Pnt(10, 10, 30), 10, 10, 10).Shape();
 
 	BRepAlgoAPI_Fuse fuser(s1, s2);
 	fuser.SetNonDestructive(true);
 	fuser.SetRunParallel(false);
 
 	fuser.Build();
+
+	assert(is_trivial_union_fuse(fuser));
+}
+
+void
+test_enclosed()
+{
+	const TopoDS_Shape
+		s1 = BRepPrimAPI_MakeBox(gp_Pnt(0, 0, 0), 20, 20, 20).Shape(),
+		s2 = BRepPrimAPI_MakeBox(gp_Pnt(5, 5, 5), 10, 10, 10).Shape();
+
+	BRepAlgoAPI_Fuse fuser(s1, s2);
+	fuser.SetNonDestructive(true);
+	fuser.SetRunParallel(false);
+
+	fuser.Build();
+
+	assert(is_enclosure_fuse(fuser));
+}
+
+void
+test_coincidence()
+{
+	const TopoDS_Shape
+		s1 = BRepPrimAPI_MakeBox(gp_Pnt(10, 10, 10), 10, 10, 11).Shape(),
+		s2 = BRepPrimAPI_MakeBox(gp_Pnt(11, 10, 20), 10, 10, 10).Shape();
+
+	BRepAlgoAPI_Fuse fuser(s1, s2);
+	fuser.SetNonDestructive(true);
+	fuser.SetRunParallel(false);
+
+	fuser.Build();
+
 	assert(fuser.IsDone());
 
-	assert (BRepTools::Write(fuser.Shape(), "tmp.brep"));
+	assert(BRepTools::Write(fuser.Shape(), "tmp.brep"));
 
-	fuser.History()->Dump(std::cout);
+	fuser.History()->Dump(std::cerr);
+
+	TopoDS_Iterator it{fuser.Shape()};
+
+	assert(it.More());
+	const TopoDS_Shape t1 = it.Value();
+	it.Next();
+
+	assert(it.More());
+	const TopoDS_Shape t2 = it.Value();
+	it.Next();
+
+	assert (!it.More());
+}
+
+int
+main()
+{
+	test_trivial_union();
+	test_enclosed();
+	test_coincidence();
 
 	return 0;
 }
