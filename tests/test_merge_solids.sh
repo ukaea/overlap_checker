a7735fbe822f3d414f4bc3e4f8e39e951ccd36c7
Sam Mason <sam@samason.uk>
2021-12-07 16:23:29 +0000

    clean up tests now it appears to be mostly working

--- tests/test_merge_solids.sh
+++ tests/test_merge_solids.sh
@@ -2,19 +2,9 @@
 
 set -euxo pipefail
 
-echo "$@"
-
-pwd
-
-env
-
-ls -l ..
-
 # assume we're running in the build directory which is a direct
 # descendant of the source
 
-./step_to_brep ../data/paramak_reactor.step paramak_reactor.brep
-
-./merge_solids paramak_reactor.brep merged.brep
+./merge_solids ../data/paramak_reactor.brep merged.brep
 
 diff -u ../data/paramak_reactor-salome_glued.brep merged.brep
